c6de3ae98f4c214b228d5ad1d12e219671faab51
AndrzejBakula <goldenwroc@gmail.com>
2020-11-03 17:38:17 +0100

    2 models ready

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+
+
+class Institution(models.Model):
+
+    INSTITUTIONS = [
+        (1, "fundacja"),
+        (2, "organizacja pozarządowa"),
+        (3, "zbiórka lokalna")
+    ]
+
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    type = models.IntegerField(choices=INSTITUTIONS, default=INSTITUTIONS[0][0])
+    categories = models.ManyToManyField(Category)
+
+
