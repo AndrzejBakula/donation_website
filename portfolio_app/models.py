87a6edfff343223212e04644045bf959cc4582b4
AndrzejBakula <goldenwroc@gmail.com>
2020-11-03 17:54:09 +0100

    3'rd model added

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -19,3 +20,15 @@ class Institution(models.Model):
     categories = models.ManyToManyField(Category)
 
 
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=256)
+    phone_number = models.CharField(max_length=24)
+    city = models.CharField(max_length=128)
+    zip_code = models.CharField(max_length=24)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
