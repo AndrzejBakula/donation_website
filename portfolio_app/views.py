561ef0d0f1c216992840c8cf4a4c5b120bf3528a
AndrzejBakula <goldenwroc@gmail.com>
2020-11-03 21:43:18 +0100

    next tasks done

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+from django.db.models import Sum
 from django.views import View
+from portfolio_app.models import Donation, Institution
 
 
 class LandingPageView(View):
     def get(self, request):
-        return render(request, "index.html")
+        bags = Donation.objects.all().aggregate(Sum("quantity"))
+        insts_count = Institution.objects.all().count()
+        ctx = {
+            "bags": bags,
+            "insts_count": insts_count
+        }
+        return render(request, "index.html", ctx)
 
 
 class LoginView(View):
